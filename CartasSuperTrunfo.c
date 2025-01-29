a4939ffa4df469fa86a42d9cb5133566bc06e17d
JosimarCTR <josimarnof@gmail.com>
2025-01-29 23:57:12 +0000

    commit1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main() {
     printf("Desafio super trunfo!\n");
 
-    char estado[3] = "rj";
+    char estado[3] = "RJ";
     char nome[20] = "rio de janeiro";
     char codigo[3] = "001";
     int populacao = 10;
@@ -11,7 +11,13 @@ int main() {
     float area = 1.75;
     float pib = 100;
 
-    
+    printf("Estado: %s\n", estado);
+    printf("Nome: %s\n", nome);
+    printf("Codigo: %s\n", codigo);
+    printf("Populacao: %d\n", populacao);
+    printf("Pontos: %d\n", pontos);
+    printf("Area: %f\n", area);
+    printf("PIB %f\n", pib);
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
