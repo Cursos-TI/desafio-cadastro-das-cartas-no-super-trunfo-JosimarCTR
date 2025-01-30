ab54bd34ec6a976a0f932a87e366058dcb729d70
JosimarCTR <josimarnof@gmail.com>
2025-01-30 23:43:03 +0000

    commit4

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
     printf("Desafio super trunfo!\n");
 
     char estado[10] = "RJ";
-    char nome[300] = "rio de janeiro";
+    char nome[300] = "Rio de janeiro";
     char codigo[300] = "001";
     int populacao = 10;
     int turisticos = 4;
@@ -16,7 +16,7 @@ int main() {
 
 
     char estado2[10] = "SP";
-    char nome2[300] = "sao paulo";
+    char nome2[300] = "Sao paulo";
     char codigo2[300] = "002";
     int populacao2 = 20;
     int turisticos2 = 5;
@@ -24,6 +24,34 @@ int main() {
     float pib2 = 200;
 
   printf("\nEstado2: %s Nome2: %s Codigo2: %s Populacao2: %d turisticos2: %d Area2: %f PIB2: %f", estado2, nome2, codigo2, populacao2, turisticos2, area2, pib2);
+
+
+   
+    char estado3[10] = "PB";
+    char nome3[300] = "Paraiba";
+    char codigo3[300] = "003";
+    int populacao3 = 30;
+    int turisticos3 = 6;
+    float area3 = 3.75;
+    float pib3 = 300;
+
+    printf("\nEstado3: %s Nome3: %s Codigo3: %s Populacao3: %d turisticos3: %d Area3: %f PIB3: %f", estado3, nome3, codigo3, populacao3, turisticos3, area3, pib3);
+
+
+    char estado4[10] = "RN";
+    char nome4[300] = "Natal";
+    char codigo4[300] = "004";
+    int populacao4 = 40;
+    int turisticos4 = 7;
+    float area4 = 4.75;
+    float pib4 = 400;
+
+    printf("\nEstado4: %s Nome4: %s Codigo4: %s Populacao4: %d turisticos4: %d Area4: %f PIB4: %f", estado4, nome4, codigo4, populacao4, turisticos4, area4, pib4);
+
+
+
+
+
 
 return 0;
 
