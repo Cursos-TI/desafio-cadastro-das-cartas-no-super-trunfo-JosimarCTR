26967d8615eb8b67e0d2d385639178f27280ba86
JosimarCTR <josimarnof@gmail.com>
2025-01-30 23:31:35 +0000

    commit3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,29 +3,32 @@
 int main() {
     printf("Desafio super trunfo!\n");
 
-    char estado[300] = "RJ";
+    char estado[10] = "RJ";
     char nome[300] = "rio de janeiro";
     char codigo[300] = "001";
     int populacao = 10;
-    int pontos = 4;
+    int turisticos = 4;
     float area = 1.75;
     float pib = 100;
 
     
-    printf("Estado: %s\n", estado);
-    printf("Nome: %s\n", nome);
-    printf("Codigo: %s\n", codigo);
-    printf("Populacao: %d\n", populacao);
-    printf("Pontos: %d\n", pontos);
-    printf("Area: %f\n", area);
-    printf("PIB %f\n", pib);
-
-    return 0;
+    printf("Estado: %s Nome: %s Codigo: %s Populacao: %d turisticos: %d Area: %f PIB %f", estado, nome, codigo, populacao, turisticos, area, pib);
 
 
-    }
+    char estado2[10] = "SP";
+    char nome2[300] = "sao paulo";
+    char codigo2[300] = "002";
+    int populacao2 = 20;
+    int turisticos2 = 5;
+    float area2 = 2.75;
+    float pib2 = 200;
 
-   
+  printf("\nEstado2: %s Nome2: %s Codigo2: %s Populacao2: %d turisticos2: %d Area2: %f PIB2: %f", estado2, nome2, codigo2, populacao2, turisticos2, area2, pib2);
+
+return 0;
+
+
+   }
 
 
 
