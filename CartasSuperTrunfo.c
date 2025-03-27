10b74c2aebe0d0af386531e1073bd802b08f4361
JosimarCTR <josimarnof@gmail.com>
2025-03-27 02:18:54 +0000

    Super trunfo novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,76 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    printf("Desafio super trunfo!\n");
+    printf("Desafio super trunfo! \n");
 
-    char estado[10] = "RJ";
-    char nome[300] = "Rio de janeiro";
-    char codigo[300] = "001";
-    int populacao = 10;
-    int turisticos = 4;
-    float area = 1.75;
-    float pib = 100;
+    printf("\nCarta A\n");
 
-    
-    printf("Estado: %s Nome: %s Codigo: %s Populacao: %d turisticos: %d Area: %f PIB %f", estado, nome, codigo, populacao, turisticos, area, pib);
+    char estado[10] = "A";
+    char cidade[30] = "Sao paulo";
+    char codigo[300] = "A01";
+    int populacao = 12325000;
+    int pontos_turisticos = 50;
+    float area = 1521.11;
+    float pib = 699.28;
 
+    printf("Estado: %s\n Cidade: %s\n Codigo: %s\n Populacao: %d\n Pontos_turisticos: %d\n Area: %f\n PIB %f \n", estado, cidade, codigo, populacao, pontos_turisticos, area, pib);
 
-    char estado2[10] = "SP";
-    char nome2[300] = "Sao paulo";
-    char codigo2[300] = "002";
-    int populacao2 = 20;
-    int turisticos2 = 5;
-    float area2 = 2.75;
-    float pib2 = 200;
+    printf("\nCarta B");
 
-  printf("\nEstado2: %s Nome2: %s Codigo2: %s Populacao2: %d turisticos2: %d Area2: %f PIB2: %f", estado2, nome2, codigo2, populacao2, turisticos2, area2, pib2);
+    char estado2[10] = "B";
+    char cidade2[30] = "Rio de Janeiro";
+    char codigo2[300] = "A02";
+    int populacao2 = 6748000;
+    int pontos_turisticos2 = 30;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
 
-
-   
-    char estado3[10] = "PB";
-    char nome3[300] = "Paraiba";
-    char codigo3[300] = "003";
-    int populacao3 = 30;
-    int turisticos3 = 6;
-    float area3 = 3.75;
-    float pib3 = 300;
-
-    printf("\nEstado3: %s Nome3: %s Codigo3: %s Populacao3: %d turisticos3: %d Area3: %f PIB3: %f", estado3, nome3, codigo3, populacao3, turisticos3, area3, pib3);
-
-
-    char estado4[10] = "RN";
-    char nome4[300] = "Natal";
-    char codigo4[300] = "004";
-    int populacao4 = 40;
-    int turisticos4 = 7;
-    float area4 = 4.75;
-    float pib4 = 400;
-
-    printf("\nEstado4: %s Nome4: %s Codigo4: %s Populacao4: %d turisticos4: %d Area4: %f PIB4: %f", estado4, nome4, codigo4, populacao4, turisticos4, area4, pib4);
-
-
-printf("\nPesquisar Estado - Nome - Codigo - Populacao - Turisticos - Area - PIB\n");
-scanf("%s %d %f", &estado, &nome, &codigo, &populacao, &turisticos, &area, &pib);
+  printf("\nEstado2: %s\n cidade2: %s\n Codigo2: %s\n Populacao2: %d\n pontos_turisticos2: %d\n Area2: %f\n PIB2: %f\n", estado2, cidade2, codigo2, populacao2, pontos_turisticos2, area2, pib2);
 
 
 
 return 0;
 
 
-   }
-
-
-
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-
+ }
